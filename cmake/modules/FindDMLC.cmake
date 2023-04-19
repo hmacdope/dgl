15bfe7f03db77561add0a46730b5fc2d87d9a4f7
hmacdope <hugomacdermott@gmail.com>
2023-04-19 14:47:29 +1000

    sort out details with METIS

---
+++ cmake/modules/FindDMLC.cmake
@@ -0,0 +1,40 @@
+# Define a CMake module to find dlpack header using find_package
+
+# Usage: find_package(DLPack [VERSION version] [REQUIRED] [QUIET])
+#
+# Searches for the dlpack header file and sets the variable
+# `DLPACK_FOUND` to `TRUE` if the header file is found,
+# and sets the variable `DLPACK_INCLUDE_DIRS` to the include directories
+# containing the header file.
+#
+# Example usage:
+#   find_package(DLPACK REQUIRED)
+# Define user options
+
+if(NOT DMLC_INCLUDE_DIR)
+  message(STATUS "DMLC_INCLUDE_DIR not defined, searching for dmlc header")  
+  find_path(DMLC_INCLUDE_DIR dmlc/base.h
+    HINTS $ENV{CONDA_PREFIX}/include
+    PATH_SUFFIXES include
+    DOC "Directory where the DMLC header files are located"
+  )
+endif()
+
+# Find dlpack header
+find_path(DMLC_INCLUDE_DIR
+    NAMES dmlc/base.h
+    PATH_SUFFIXES include
+)
+
+# Check if dlpack header is found
+if(DMLC_INCLUDE_DIR)
+    set(DMLC_FOUND TRUE)
+else()
+    set(DMLC_FOUND FALSE)
+endif()
+
+# Standard package handling
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(DMLC REQUIRED_VARS DLPACK_INCLUDE_DIR)
+
+mark_as_advanced(DMLC_INCLUDE_DIR)
